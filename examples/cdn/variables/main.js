19c6114449ca9ffc447b89287be7ad35c2febd37
David Longworth <dave@abovebored.co.uk>
2017-01-04 17:35:32 +0000

    CDN demo update

--- examples/cdn/variables/main.js
+++ examples/cdn/variables/main.js
@@ -1,61 +1,105 @@
 $(document).ready(function() {
   $('body').on('click', '.js-addprop', function(){
-    // Create html
-    $new = $('<li>' + $('#addprop').html() + '</li>');
-    $('.settings ul').append($new);
-
-    // Create selectize
-    $select = $new.find('select').selectize({
-      maxItems: 1,
-      onInitialize: function() {
-        $select.focus();
-      }
-    });
+    createSelectBox();
   });
   
   $('body').on('click', '.js-killprop', function(){
-    var $prop = $(this).parent();
-
-    $prop.remove();
-
-    // function to update image
+    $(this).parent().remove();
+    updateImage();
   });
 
-  // Selectize
-  /*var xhr;
-  var select_state, $select_state;
-  var select_city, $select_city;
+  $('body').on('click', '.js-update', function(){
+    updateImage();
+  });
 
-  $select_state = $('#select-state').selectize({
-    onChange: function(value) {
-      if (!value.length) return;
-      select_city.disable();
-      select_city.clearOptions();
-      select_city.load(function(callback) {
-        xhr && xhr.abort();
-        xhr = $.ajax({
-          url: './options/' + value + '.json',
-          success: function(results) {
-            select_city.enable();
-            callback(results);
-          },
-          error: function() {
-            callback();
-          }
-        })
+  $('body').on('click', '.sources a', function(){
+    $('.source').addClass('hide');
+    $($(this).attr('href')).removeClass('hide');
+    $('.sources a').removeClass('active');
+    $(this).addClass('active');
+
+    return false;
+  });
+
+  function parseImage() {
+    var startUrl = $('.image img').attr('src'),
+        url = new URI(startUrl),
+        path = url._parts.path.split('/'),
+        hostname = url._parts.hostname,
+        source = path.slice(0,-1).join('/').substring(1),
+        file = path.pop(-1),
+        query = URI.parseQuery(url._parts.query),
+        queryLen = Object.keys(query).length,
+        qs = '',
+        i = 0;
+
+    $('.settings li.active').remove();
+
+    $.each(query, function(key, value){
+      qs += '<span style="color:#fff">' + key + '</span>=<span style="color:#44BDEC">' + value + '</span>';
+      if (i != (queryLen -1)) qs += '&'; i++;
+    });
+
+    $.each(query, function(key, value){
+      createSelectBox(key, value);
+    });
+
+    $('.url').html(
+      url._parts.protocol + '://' +
+      '<span style="color:#ECB344">' + hostname  + '</span>/' +
+      '<span style="color:#C969F5">' + source + '</span>/' +
+      '<span style="color:#FF5252">' + file + '</span>?' +
+      qs
+    );
+
+    $('#imgDomain').text(hostname);
+    $('#imgSrc').text(source);
+    $('#imgFile').text(file);
+  }
+
+  parseImage();
+
+  function createSelectBox(key, value) {
+    var $select, $new;
+
+    // Create html
+    $new = $('<li class="active">' + $('#addprop').html() + '</li>');
+    $('.settings ul').append($new);
+
+    // Set the key
+    $new.find('.theKey').val(key);
+
+    // Set the value
+    $new.find('.theValue').val(value);
+
+    $new.find('.theKey').on('change', function() {
+      $new.find('.prop-type').addClass('hide');
+      $new.find('.prop-type[data-type="'+this.value+'"]').removeClass('hide');
+      $new.find('.theValue').focus();
+    }).trigger('change');
+  }
+
+  function updateImage() {
+    var uri = 'http://' + $('#imgDomain').text() + '/' + $('#imgSrc').text() + '/' + $('#imgFile').text(),
+        url = new URI(uri);
+
+    $('.spinner').removeClass('hide');
+
+    $('.settings li.active').each(function() {
+      var theKey = $(this).find('.theKey').val(),
+          theValue = $(this).find('.prop-type').not('.hide').find('.theValue').val();
+
+      url.addSearch(theKey, theValue);
+    });
+
+    $('.image img')
+      .addClass('loading')
+      .attr('src', url.toString())
+      .one('load', function() {
+        $('.spinner').addClass('hide');
+        $(this).removeClass('loading')
       });
-    }
-  });
 
-  $select_city = $('#select-city').selectize({
-    valueField: 'name',
-    labelField: 'name',
-    searchField: ['name']
-  });
-
-  select_city  = $select_city[0].selectize;
-  select_state = $select_state[0].selectize;
-
-  select_city.disable();*/
-
+    parseImage();
+  }
 });
