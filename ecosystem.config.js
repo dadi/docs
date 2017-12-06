1e01bd954d8377a352946377b730b53285652658
James Lambie <jameslambie@gmail.com>
2017-12-06 14:20:11 +0000

    chore: staging version

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,18 +3,21 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps : [
+  apps: [
 
-    // First application
     {
-      name      : 'DADI DOCS',
-      script    : 'server.js',
+      name: 'DADI DOCS',
+      script: 'server.js',
       env: {
         COMMON_VARIABLE: 'true'
       },
-      env_production : {
+      env_staging: {
+        PORT: 8000,
+        NODE_ENV: 'staging'
+      },
+      env_production: {
         NODE_ENV: 'production'
       }
     }
   ]
-};
+}
