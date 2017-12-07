7ad7dde3b3674435c5ae754293a06534ac5f0efc
James Lambie <jameslambie@gmail.com>
2017-12-07 10:54:10 +0000

    chore: remove staging config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,16 +4,11 @@ module.exports = {
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
   apps: [
-
     {
       name: 'DADI DOCS',
       script: 'server.js',
       env: {
         COMMON_VARIABLE: 'true'
-      },
-      env_staging: {
-        PORT: 8000,
-        NODE_ENV: 'staging'
       },
       env_production: {
         NODE_ENV: 'production'
