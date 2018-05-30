ab4b2f8fe61ea02f386a98df3d11f3fcd1bb2f5e
David Longworth <david@longworth.to>
2018-05-30 10:53:20 +0100

    feat: latest is always at root

--- workspace/events/navigation.js
+++ workspace/events/navigation.js
@@ -2,51 +2,15 @@ const toc = require('markdown-toc')
 const marked = require('marked')
 
 const Event = function (req, res, data, callback) {
-  let navigation = []
-  let processed = 0
+  if (data.hasResults('doc') && data.doc.results[0].contentText) {
+    const map = toc(data.doc.results[0].contentText, { maxdepth: 6 }).content
+      .replace(/]\(\#/gmi, '](' + '' + '#')
+      .replace(/`/gmi, '')
 
-  data.docs.results.forEach((doc, index) => {
-    processed++
+    data.navigation = marked(map)
+  }
 
-    // Add the app id to the links e.g., #api/...
-    let docObj = {
-      name: doc.attributes.title,
-      product: doc.attributes.product,
-      version: doc.attributes.version,
-      id: doc.attributes._id,
-      order: doc.attributes.order
-    }
-
-    if (data.product_doc && data.product_doc.results[0]) {
-      let productDocument = data.product_doc.results[0]
-
-      let map = toc(doc.contentText, { maxdepth: 6 }).content
-        .replace(/]\(\#/gmi, '](' + '' + '#')
-        .replace(/`/gmi, '')
-
-      docObj.tocMap = marked(map)
-
-      data.product_doc.results[0] = productDocument
-    }
-
-    // Only add latest version to navigation
-    if (!doc.attributes.version || doc.attributes.version === 'latest') {
-      navigation.push(docObj)
-    }
-
-    // If done
-    if (processed === data.docs.results.length) {
-      callback(null, navigation)
-    }   
-  })
-}
-
-function isRequestedProduct (product, currentDocument) {
-  return currentDocument.attributes.product === product.attributes.product
-}
-
-function isRequestedVersion (product, currentDocument) {
-  return currentDocument.attributes.version === product.attributes.version
+  callback()
 }
 
 module.exports = function (req, res, data, callback) {
