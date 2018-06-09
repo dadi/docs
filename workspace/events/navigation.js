aaaa6a40010a3f1adbea37a1505a16e5c9fe42d5
James Lambie <jameslambie@gmail.com>
2018-06-09 14:16:16 +1000

    feat: use specific version numbers

--- workspace/events/navigation.js
+++ workspace/events/navigation.js
@@ -10,6 +10,33 @@ const Event = function (req, res, data, callback) {
     data.navigation = marked(map)
   }
 
+  // Sort the versions
+  data.versions.results.sort((a, b) => {
+    let aParts = a.attributes.version.split('.')
+    let bParts = b.attributes.version.split('.')
+
+    if (parseInt(aParts[0]) > parseInt(bParts[0])) return 1
+    if (parseInt(aParts[0]) < parseInt(bParts[0])) return -1
+
+    if (parseInt(aParts[0]) === parseInt(bParts[0])) {
+      if (parseInt(aParts[1]) > parseInt(bParts[1])) return 1
+      if (parseInt(aParts[1]) < parseInt(bParts[1])) return -1
+      if (parseInt(aParts[1]) === parseInt(bParts[1])) return 0
+    }
+  })
+
+  // Latest version from the loaded documents
+  data.latestVersion = data.versions.results[data.versions.results.length - 1].attributes.version
+
+  // Redirect to latest if no version is pecified
+  if (!data.hasResults('doc')) {
+    res.writeHead(302, {
+      Location: `${data.url.pathname}${data.latestVersion}`
+    })
+
+    res.end()
+  }
+
   callback()
 }
 
