36b9544bb623bf2d66a54951566c32604bf04e1f
James Lambie <jameslambie@gmail.com>
2018-06-12 13:53:07 +1200

    fix: merge 404 pages with version redirects

--- workspace/events/navigation.js
+++ workspace/events/navigation.js
@@ -2,8 +2,37 @@ const toc = require('markdown-toc')
 const marked = require('marked')
 
 const Event = function (req, res, data, callback) {
+  if (data.hasResults('versions')) {
+    // Sort the versions
+    data.versions.results.sort((a, b) => {
+      let aParts = a.attributes.version.toString().split('.')
+      let bParts = b.attributes.version.toString().split('.')
+
+      if (parseInt(aParts[0]) > parseInt(bParts[0])) return 1
+      if (parseInt(aParts[0]) < parseInt(bParts[0])) return -1
+
+      if (parseInt(aParts[0]) === parseInt(bParts[0])) {
+        if (parseInt(aParts[1]) > parseInt(bParts[1])) return 1
+        if (parseInt(aParts[1]) < parseInt(bParts[1])) return -1
+        if (parseInt(aParts[1]) === parseInt(bParts[1])) return 0
+      }
+    })
+
+    // Latest version from the loaded documents
+    data.latestVersion = data.versions.results[data.versions.results.length - 1].attributes.version
+
+    // Redirect to latest if no version is specified
+    if (!data.hasResults('doc')) {
+      res.writeHead(302, {
+        Location: `${data.url.pathname}${data.latestVersion}`
+      })
+
+      return res.end()
+    }
+  }
+
   if (data.hasResults('doc') && data.doc.results[0].contentText) {
-    const map = toc(data.doc.results[0].contentText, { maxdepth: 6 }).content
+    let map = toc(data.doc.results[0].contentText, { maxdepth: 6 }).content
       .replace(/]\(\#/gmi, '](' + '' + '#')
       .replace(/`/gmi, '')
 
@@ -13,33 +42,6 @@ const Event = function (req, res, data, callback) {
     error.statusCode = 404
 
     return callback(error)
-  }
-
-  // Sort the versions
-  data.versions.results.sort((a, b) => {
-    let aParts = a.attributes.version.toString().split('.')
-    let bParts = b.attributes.version.toString().split('.')
-
-    if (parseInt(aParts[0]) > parseInt(bParts[0])) return 1
-    if (parseInt(aParts[0]) < parseInt(bParts[0])) return -1
-
-    if (parseInt(aParts[0]) === parseInt(bParts[0])) {
-      if (parseInt(aParts[1]) > parseInt(bParts[1])) return 1
-      if (parseInt(aParts[1]) < parseInt(bParts[1])) return -1
-      if (parseInt(aParts[1]) === parseInt(bParts[1])) return 0
-    }
-  })
-
-  // Latest version from the loaded documents
-  data.latestVersion = data.versions.results[data.versions.results.length - 1].attributes.version
-
-  // Redirect to latest if no version is pecified
-  if (!data.hasResults('doc')) {
-    res.writeHead(302, {
-      Location: `${data.url.pathname}${data.latestVersion}`
-    })
-
-    res.end()
   }
 
   callback()
--- workspace/events/search.js
+++ workspace/events/search.js
@@ -6,23 +6,25 @@ const Event = function (req, res, data, callback) {
   let ds = data.hasResults('search') ? data.search : data.doc
   let results = []
 
-  ds.results.forEach(doc => {
-    let map = toc(doc.contentText, { maxdepth: 6 }).content
-      .replace(/]\(\#/gmi, '](' + '' + '#')
-      .replace(/`/gmi, '')
+  if (ds) {
+    ds.results.forEach(doc => {
+      let map = toc(doc.contentText, { maxdepth: 6 }).content
+        .replace(/]\(\#/gmi, '](' + '' + '#')
+        .replace(/`/gmi, '')
 
-    let nav = marked(map)
-    let $ = cheerio.load(nav)
+      let nav = marked(map)
+      let $ = cheerio.load(nav)
 
-    $('li').each((idx, el) => {
-      results.push({
-        product: doc.attributes.product,
-        productName: doc.attributes.title,
-        text: $(el).text().split(/\n/)[0],
-        url: $(el).find('a').attr('href')
+      $('li').each((idx, el) => {
+        results.push({
+          product: doc.attributes.product,
+          productName: doc.attributes.title,
+          text: $(el).text().split(/\n/)[0],
+          url: $(el).find('a').attr('href')
+        })
       })
     })
-  })
+  }
 
   data.search = JSON.stringify(results)
 
